3148eb33ad5be8c3f3529068e57fc41caf4c53d1
Rupesh <rupesh3101.kadam@gmail.com>
2020-04-09 18:51:06 +0530

    Second Commit

---
+++ src/main/java/TestRunner/Runner.java
@@ -0,0 +1,21 @@
+package TestRunner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = {"D:\\Practise\\CucumberPOM\\src\\main\\java\\Features\\Login.feature"}
+		,glue = {"stepDefinition"},
+		format = {"pretty","html:test-output", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"},
+		dryRun = true,
+		monochrome = true,
+		strict = true
+//		tags = {"@End2End"}
+		)
+
+public class Runner {
+
+}
